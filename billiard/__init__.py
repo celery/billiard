994ab91dfae9971dddbb1fbc90f753630da175a2
Ask Solem <askh@opera.com>
2009-11-16 16:32:53 +0100

    Initial import

---
+++ billiard/__init__.py
@@ -0,0 +1,7 @@
+"""Multiprocessing Task Pool"""
+VERSION = (0, 0, 1)
+__version__ = ".".join(map(str, VERSION))
+__author__ = "Opera Software (WebTeam)"
+__contact__ = "portal-dev@list.opera.com"
+__homepage__ = "http://cgit.portal-dev.oslo.osa/cgit.cgi/billiard/"
+__docformat__ = "restructuredtext"
