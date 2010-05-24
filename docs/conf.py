27a7f6358669c2ad48fcb6ee5e16118c7b1cda4a
Ask Solem <askh@opera.com>
2010-05-24 20:37:55 +0200

    Fixed typo in copyright. Closes #1. Thanks to fladi

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'billiard'
-copyright = u'2009, Opera Softare (WebTeam)'
+copyright = u'2009-2010, Ask Solem'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
