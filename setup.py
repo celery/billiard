3d63cf067b943b37eb4c451c5424d2f086e7a6e1
Ask Solem <ask@celeryproject.org>
2012-07-21 09:32:04 +0100

    Support for FreeBSD 7+. Thanks to @koobs

--- setup.py
+++ setup.py
@@ -76,13 +76,20 @@ elif sys.platform.startswith('cygwin'): # Cygwin
         HAVE_BROKEN_SEM_UNLINK=1
         )
     libraries = []
-elif sys.platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8'):
+elif sys.platform in ('freebsd4', 'freebsd5', 'freebsd6'):
     # FreeBSD's P1003.1b semaphore support is very experimental
     # and has many known problems. (as of June 2008)
-    macros = dict(                  # FreeBSD
+    macros = dict(                  # FreeBSD 4-6
         HAVE_SEM_OPEN=0,
         HAVE_SEM_TIMEDWAIT=0,
         HAVE_FD_TRANSFER=1,
+        )
+    libraries = []
+elif sys.platform in ('freebsd7', 'freebsd8', 'freebsd9', 'freebsd10'):
+    macros = dict(                  # FreeBSD 7+
+            HAVE_SEM_OPEN=1,
+            HAVE_SEM_TIMEDWAIT=1,
+            HAVE_FD_TRANSFER=1,
         )
     libraries = []
 elif sys.platform.startswith('openbsd'):
