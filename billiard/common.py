043b8e1d04809ce07f2c3831f15fcd394852f058
Ask Solem <ask@celeryproject.org>
2013-04-15 16:38:46 +0100

    restart_state was not properly resetting time passed.  Fixes celery/celery#1306

--- billiard/common.py
+++ billiard/common.py
@@ -54,15 +54,17 @@ class restart_state(object):
 
     def step(self):
         now = time()
-        R = self.R
         if self.T and now - self.T >= self.maxT:
-            self.R = 0
-        elif self.maxR and R >= self.maxR:
+            # maxT passed, reset counter and time passed.
+            self.T, self.R = now, 0
+        elif self.maxR and self.R >= self.maxR:
             # verify that R has a value as it may have been reset
             # by another thread, and we want to avoid locking.
             if self.R:
                 raise self.RestartFreqExceeded(
                     "%r in %rs" % (self.R, self.maxT),
                 )
+        # first run sets T
+        if self.T is None:
+            self.T = now
         self.R += 1
-        self.T = now
