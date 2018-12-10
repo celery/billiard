8f613d2d75a185e3e7db17367c0c7af4f407ecaf
Matus Valo <matusvalo@gmail.com>
2018-12-10 06:15:38 -0800

    Improve unittests of pdeathsig

--- t/unit/test_spawn.py
+++ t/unit/test_spawn.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 
 import sys
-from billiard import get_context, Value, Process
-from billiard.util import set_pdeathsig
+from billiard import get_context, Value, Process, Event
+from billiard.util import set_pdeathsig, get_pdeathsig
 import pytest
 import psutil
 import signal
@@ -24,18 +24,28 @@ class test_spawn:
         p = Process(target=parent_task, args=(return_pid,))
         p.start()
         p.join()
+        sleep(1)    # We need to wait to have child process killed.
         with pytest.raises(psutil.NoSuchProcess):
             proc = psutil.Process(return_pid.value)
 
-def child_process():
+    def test_set_get_pdeathsig(self):
+        sig = get_pdeathsig()
+        assert sig == 0
+        set_pdeathsig(signal.SIGTERM)
+        sig = get_pdeathsig()
+        assert sig == signal.SIGTERM
+
+def child_process(child_started):
     set_pdeathsig(signal.SIGTERM)
+    child_started.set()
     while True:
         sleep(1)
 
 def parent_task(return_pid):
-    p = Process(target=task_from_process, args=('opa',))
+    child_started = Event()
+    p = Process(target=child_process, args=(child_started,))
     p.start()
-    sleep(1)
+    child_started.wait()
     return_pid.value = p.pid
 
 def task_from_process(name):
