28ba0c1285ab6ac624346e36cf02ce1e1a4320aa
Matus Valo <matusvalo@gmail.com>
2018-12-10 04:55:00 -0800

    Added unittest for set_pdeathsig()

--- t/unit/test_spawn.py
+++ t/unit/test_spawn.py
@@ -1,6 +1,12 @@
 from __future__ import absolute_import
 
-from billiard import get_context
+import sys
+from billiard import get_context, Value, Process
+from billiard.util import set_pdeathsig
+import pytest
+import psutil
+import signal
+from time import sleep
 
 class test_spawn:
     def test_start(self):
@@ -10,6 +16,27 @@ class test_spawn:
         p.start()
         p.join()
         return p.exitcode
+
+    @pytest.mark.skipif(not sys.platform.startswith('linux'),
+                        reason='set_pdeathsig() is supported only in Linux')
+    def test_set_pdeathsig(self):
+        return_pid = Value('i')
+        p = Process(target=parent_task, args=(return_pid,))
+        p.start()
+        p.join()
+        with pytest.raises(psutil.NoSuchProcess):
+            proc = psutil.Process(return_pid.value)
+
+def child_process():
+    set_pdeathsig(signal.SIGTERM)
+    while True:
+        sleep(1)
+
+def parent_task(return_pid):
+    p = Process(target=task_from_process, args=('opa',))
+    p.start()
+    sleep(1)
+    return_pid.value = p.pid
 
 def task_from_process(name):
     print('proc:', name)
