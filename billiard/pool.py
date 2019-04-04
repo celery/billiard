823caf5a22cba09a180166e92721f022b0375901
Elmar Pruesse <epruesse@users.noreply.github.com>
2019-04-04 16:47:12 -0600

    Fix win&py2k

--- billiard/pool.py
+++ billiard/pool.py
@@ -277,6 +277,8 @@ class Worker(object):
 
         def exit(status=None):
             _exitcode[0] = status
+            if sys.platform == 'win32' and sys.version_info < (3,0)
+                return _exit()
             return _exit(status)
         sys.exit = exit
 
