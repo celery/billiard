13922230092a4191583e592e9c6d49f912a3fd03
Asif Saif Uddin <auvipy@gmail.com>
2023-11-06 11:21:31 +0600

    Bump version: 4.1.0 → 4.2.0

--- billiard/__init__.py
+++ billiard/__init__.py
@@ -21,7 +21,7 @@
 import sys
 from . import context
 
-VERSION = (4, 1, 0)
+VERSION = (4, 2, 0)
 __version__ = '.'.join(map(str, VERSION[0:4])) + "".join(VERSION[4:])
 __author__ = 'R Oudkerk / Python Software Foundation'
 __author_email__ = 'python-dev@python.org'
