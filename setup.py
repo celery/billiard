6db9461b236729a3dde325824629ac07c111c2fb
Asif Saif Uddin <auvipy@gmail.com>
2023-11-06 11:20:39 +0600

    added python 3.12 in classifier

--- setup.py
+++ setup.py
@@ -213,7 +213,7 @@ def run_setup(with_extensions=True):
             'Programming Language :: Python',
             'Programming Language :: C',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.12',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
